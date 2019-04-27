479a30527c0994681c54f731ac5bee8ce1454fc8
孙晶 <sunjing@sinobest.cn>
2019-04-27 14:53:36 +0800

    feat(money): 制造对象

--- currency/src/main/java/cn/tofdragon/currency/Money.java
+++ currency/src/main/java/cn/tofdragon/currency/Money.java
@@ -4,13 +4,23 @@ package cn.tofdragon.currency;
  *
  * @author sunjing
  */
-public class Money {
+public abstract class Money {
 
     protected int amount;
+
+    public static Dollar dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    public static Franc franc(int amount) {
+        return new Franc(amount);
+    }
 
     @Override
     public boolean equals(Object obj) {
         Money money = (Money) obj;
         return amount == money.amount && getClass().equals(obj.getClass());
     }
+
+    public abstract Money times(int multiplier) ;
 }
