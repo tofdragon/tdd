3abbfbaabef6355d39bfedcb26099c2d693c0b69
孙晶 <sunjing@sinobest.cn>
2019-06-01 17:28:19 +0800

    refactor(rule): use factory create rule

--- foobarqix/src/main/java/com/kata/FooBarQix.java
+++ foobarqix/src/main/java/com/kata/FooBarQix.java
@@ -18,15 +18,15 @@ public final class FooBarQix {
     }
 
     private void createRule() {
-        rule = new DivisibleRule(3, "Foo");
-        Rule fiveDivisible = new DivisibleRule(5,"Bar");
-        Rule sevenDivisible = new DivisibleRule(7, "Qix");
+        rule = RuleFactory.divisible(3, "Foo");
+        Rule fiveDivisible =  RuleFactory.divisible(5,"Bar");
+        Rule sevenDivisible =  RuleFactory.divisible(7, "Qix");
 
-        Rule threeContains =  new ContainsRule(3, "Foo");
-        Rule fiveContains = new ContainsRule(5, "Bar");
-        Rule sevenContains = new ContainsRule(7, "Qix");
+        Rule threeContains =   RuleFactory.contains(3, "Foo");
+        Rule fiveContains = RuleFactory.contains(5, "Bar");
+        Rule sevenContains = RuleFactory.contains(7, "Qix");
 
-        Rule originalNumber = new OriginalNumberRule();
+        Rule originalNumber = RuleFactory.originalNumber();
 
         rule.nextRule(fiveDivisible);
         fiveDivisible.nextRule(sevenDivisible);
