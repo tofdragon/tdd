70f42d64406c593b069ed5d9402e3e5904bc4b40
孙晶 <sunjing@sinobest.cn>
2020-06-27 10:13:26 +0800

    refactor(code): extract method

--- tdd-java/tdd-java-ch03-tic-tac-toe/src/main/java/com/packtpublishing/tddjava/ch03tictactoe/TicTacToe.java
+++ tdd-java/tdd-java-ch03-tic-tac-toe/src/main/java/com/packtpublishing/tddjava/ch03tictactoe/TicTacToe.java
@@ -12,16 +12,20 @@ public class TicTacToe {
     private Character[][] board = {{'\0', '\0', '\0'}, {'\0', '\0', '\0'}, {'\0', '\0', '\0'}};
 
     public void play(int x, int y) {
-        if (x < 1 || x > 3) {
-            throw
-                    new RuntimeException("X is outside board");
-        } else if (y < 1 || y > 3) {
-            throw
-                    new RuntimeException("Y is outside board");
+        checkAxis(x);
+        checkAxis(y);
+        setBox(x, y);
+    }
+
+    private void checkAxis(int axis) {
+        if (axis <1 || axis > 3) {
+            throw new RuntimeException("X is outside board");
         }
+    }
+
+    private void setBox(int x, int y) {
         if (board[x - 1][y - 1] != '\0') {
-            throw
-                    new RuntimeException("Box is occupied");
+            throw new RuntimeException("Box is occupied");
         } else {
             board[x - 1][y - 1] = 'X';
         }
