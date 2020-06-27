009392bc932d1da08833a31d667b23deee597867
孙晶 <sunjing@sinobest.cn>
2020-06-27 10:02:34 +0800

    feat(x): whenXOutsideBoardThenRuntimeException

--- tdd-java/tdd-java-ch03-tic-tac-toe/src/main/java/com/packtpublishing/tddjava/ch03tictactoe/TicTacToe.java
+++ tdd-java/tdd-java-ch03-tic-tac-toe/src/main/java/com/packtpublishing/tddjava/ch03tictactoe/TicTacToe.java
@@ -5,4 +5,10 @@ package com.packtpublishing.tddjava.ch03tictactoe;
  * @author sunjing
  */
 public class TicTacToe {
+
+    public void play(int x, int y) {
+        if (x < 1 || x > 3) {
+            throw new RuntimeException();
+        }
+    }
 }
--- tdd-java/tdd-java-ch03-tic-tac-toe/src/test/java/com/packtpublishing/tddjava/ch03tictactoe/TicTacToeSpec.java
+++ tdd-java/tdd-java-ch03-tic-tac-toe/src/test/java/com/packtpublishing/tddjava/ch03tictactoe/TicTacToeSpec.java
@@ -1,8 +1,30 @@
 package com.packtpublishing.tddjava.ch03tictactoe;
 
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
 /**
  *
  * @author sunjing
  */
 public class TicTacToeSpec {
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
+    private TicTacToe ticTacToe;
+
+    @Before
+    public final void before() {
+        ticTacToe = new TicTacToe();
+    }
+
+    @Test
+    public void whenXOutsideBoardThenRuntimeException()
+    {
+        exception.expect(RuntimeException.class);
+        ticTacToe.play(5, 2);
+    }
 }
