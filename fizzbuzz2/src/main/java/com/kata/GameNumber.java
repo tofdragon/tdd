0cd198a364d7a8bcf056f0592a4c14e69cbf9b40
孙晶 <sunjing@sinobest.cn>
2020-11-14 21:19:24 +0800

    test(fizzbuzz3): say number + fizz + buzz + fizzbuzz

--- fizzbuzz2/src/main/java/com/kata/GameNumber.java
+++ fizzbuzz2/src/main/java/com/kata/GameNumber.java
@@ -4,4 +4,27 @@ package com.kata;
  * @author sunjing
  */
 public class GameNumber {
+
+    private int rawNumber;
+
+    public GameNumber(int rowNumber) {
+        this.rawNumber =  rowNumber;
+    }
+
+    @Override
+    public String toString() {
+        if (rawNumber % 3 == 0 && rawNumber % 5 == 0) {
+            return "fizzbuzz";
+        }
+
+        if (rawNumber % 3 == 0) {
+            return "fizz";
+        }
+
+        if (rawNumber % 5 == 0) {
+            return "buzz";
+        }
+
+        return String.valueOf(this.rawNumber);
+    }
 }
--- fizzbuzz2/src/test/java/com/kata/GameNumberTest.java
+++ fizzbuzz2/src/test/java/com/kata/GameNumberTest.java
@@ -2,13 +2,39 @@ package com.kata;
 
 import org.junit.Test;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 /**
  * @author sunjing
  */
 public class GameNumberTest {
 
     @Test
-    public void should_create_game_number_from_raw_number() {
-        GameNumber gameNumber = new GameNumber();
+    public void should_say_1_when_row_number_is_1() {
+        GameNumber gameNumber = new GameNumber(1);
+
+        assertThat(gameNumber.toString(), is("1"));
+    }
+
+    @Test
+    public void should_say_fizz_when_row_number_is_3() {
+        GameNumber gameNumber = new GameNumber(3);
+
+        assertThat(gameNumber.toString(), is("fizz"));
+    }
+
+    @Test
+    public void should_say_buzz_when_row_number_is_5() {
+        GameNumber gameNumber = new GameNumber(5);
+
+        assertThat(gameNumber.toString(), is("buzz"));
+    }
+
+    @Test
+    public void should_say_fizzbuzz_when_row_number_is_15() {
+        GameNumber gameNumber = new GameNumber(15);
+
+        assertThat(gameNumber.toString(), is("fizzbuzz"));
     }
 }
