78687255a9b186fae7d2c113b76a86bdb991b4e5
孙晶 <sunjing@sinobest.cn>
2020-11-14 21:03:01 +0800

    feat(init): fizzbuzz study

---
+++ fizzbuzz2/src/test/java/com/kata/GameNumberTest.java
@@ -0,0 +1,14 @@
+package com.kata;
+
+import org.junit.Test;
+
+/**
+ * @author sunjing
+ */
+public class GameNumberTest {
+
+    @Test
+    public void should_create_game_number_from_raw_number() {
+
+    }
+}
