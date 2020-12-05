a4a7d3c6b1404414ac755555c0149cb11cb68855
孙晶 <sunjing@sinobest.cn>
2020-12-05 23:08:53 +0800

    test(fizzbuzz2): create from raw

--- fizzbuzz2/src/main/java/com/kata/GameNumber.java
+++ fizzbuzz2/src/main/java/com/kata/GameNumber.java
@@ -5,4 +5,16 @@ package com.kata;
  * @author sunjing
  */
 public class GameNumber {
+
+    private int raw;
+
+    public GameNumber(int raw) {
+        this.raw = raw;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(raw);
+    }
+
 }
--- fizzbuzz2/src/test/java/com/kata/GameNumberTest.java
+++ fizzbuzz2/src/test/java/com/kata/GameNumberTest.java
@@ -2,13 +2,17 @@ package com.kata;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+
 /**
  * @author sunjing
  */
 public class GameNumberTest {
 
     @Test
-    public void should_create_game_number() {
-        GameNumber gameNumber = new GameNumber();
+    public void should_create_game_number_from_raw_number() {
+        GameNumber gameNumber = new GameNumber(1);
+        assertThat(gameNumber.toString(), is("1"));
     }
 }
